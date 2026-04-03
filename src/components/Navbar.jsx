ddb567294a46bca7b091a1098f111f0b788a43a5
Guillermo de la Torre Isunza <memo2507.delatorre@gmail.com>
2026-04-02 18:57:37 -0600

    Agregando el footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { NavLink } from "react-router";
 
 const Navbar = ({ children }) => {
     return (
-        <>
+        <div className='layout'>
             <div className="container-navbar">
 
                 <div className="nav-title">
@@ -19,14 +19,33 @@ const Navbar = ({ children }) => {
                     <NavLink to="/projects">Projects</NavLink>
                     <NavLink to="/contactMe">Contact Me</NavLink>
                 </div>
-                
+
                 <div className="resume">
                     <button className="resume-btn">Resume</button>
                 </div>
 
             </div>
-            {children}
-        </>
+
+            <main className="page-content">
+                {children}
+            </main>
+
+            <footer className="footer">
+                <div className="footer-left">
+                    MEMO'S_PORTFOLIO
+                </div>
+
+                <div className="footer-center">
+                    © 2026 Memo's Portfolio.
+                </div>
+
+                <div className="footer-right">
+                    <a href="https://github.com/Guillermo-dlti" target="_blank" rel="noopener noreferrer">Github</a>
+                    <a href="https://www.linkedin.com/in/guillermo-de-la-torre-isunza/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                    <a href="mailto:guillermo.delatorreisunza@gmail.com">Email</a>
+                </div>
+            </footer>
+        </div>
     )
 }
 
