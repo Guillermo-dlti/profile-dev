5b0cf4fee8628ecbd76ac2d077008d2430ed9169
Guillermo de la Torre Isunza <memo2507.delatorre@gmail.com>
2026-04-09 23:59:13 -0600

    Fotos

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -4,6 +4,9 @@ import ProjectCard from "./ProjectCard";
 import projectLarge from "../assets/images/project-large.jpg";
 import projectServers from "../assets/images/project-servers.jpg";
 import projectCode from "../assets/images/project-code.jpg";
+import smartmirror1 from "../assets/images/smartmirror1.webp";
+import smartcities1 from "../assets/images/smartcities1.webp";
+import pokemon1 from "../assets/images/pokemon1.webp";
 
 export default function ProjectsSection() {
     return (
@@ -27,7 +30,7 @@ export default function ProjectsSection() {
                         title="Banorte Ruta Consciente"
                         description="A Smart Cities fintech solution developed for Hack MTY 2025 that bridges financial services with urban mobility. The platform utilizes predictive data models and TomTom’s Map Toolkit to incentivize sustainable commuting habits by rewarding users for responsible travel decisions. Built with a Python-Flask backend and a React frontend, it explores how banking data can empower greener, more connected urban environments."
                         tags={["PYTHON", "FLASK", "SCIKIT-LEARN", "TOMTOM API", "REACT", "MONGODB"]}
-                        image={projectLarge}
+                        image={smartcities1}
                         showAction={true}
                         repoUrl="https://devpost.com/software/banorte-ruta-consciente"
                     />
@@ -37,7 +40,7 @@ export default function ProjectsSection() {
                         title="Smart Mirror IoT"
                         description="Award-winning integrated system featuring real-time AI facial recognition via OpenCV and a robust IoT sensor network managed by Raspberry Pi and NodeMCU. Developed for Expo Ingenierías 2025, it leverages a hybrid SQL/NoSQL architecture to handle user data and environmental telemetry. Winner of 1st Place in Physical Prototype."
                         tags={["NODE.JS", "REACT", "PYTHON", "IOT", "MONGODB"]}
-                        image={projectServers}
+                        image={smartmirror1}
                         showAction={true}
                         actionText="View Repository"
                         repoUrl="https://github.com/Guillermo-dlti/Smart-Mirror"
@@ -60,7 +63,7 @@ export default function ProjectsSection() {
                         title="Nintendo Switch"
                         description="A Nintendo Switch interface simulation built with React and Tailwind CSS. It features a dynamic gaming dashboard that integrates the PokeAPI to manage real-time data fetching, sprite rendering, and interactive character modules."
                         tags={["REACT", "TAILWIND", "REST API", "UX/UI"]}
-                        image={projectCode}
+                        image={pokemon1}
                         showAction={true}
                         actionText="View Repository"
                         repoUrl="https://github.com/Guillermo-dlti/project-console-retro-game"
